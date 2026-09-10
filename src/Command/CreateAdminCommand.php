53b928bb59a11ab66e46df2cecd6ce827b0df306
sylpi10 <syl.pillet@hotmail.fr>
2026-09-11 00:42:50 +0200

    creation du user admin et form de connexion

---
+++ src/Command/CreateAdminCommand.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+#[AsCommand(
+    name: 'app:create-admin',
+    description: 'Create or update an admin user',
+)]
+class CreateAdminCommand extends Command
+{
+    public function __construct(
+        private readonly EntityManagerInterface $entityManager,
+        private readonly UserRepository $userRepository,
+        private readonly UserPasswordHasherInterface $passwordHasher,
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('username', InputArgument::REQUIRED, 'The admin username')
+            ->addArgument('password', InputArgument::OPTIONAL, 'The admin password (prompted if omitted)')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $username = $input->getArgument('username');
+
+        $password = $input->getArgument('password');
+        if (!$password) {
+            $password = $io->askHidden('Password');
+        }
+
+        $user = $this->userRepository->findOneBy(['username' => $username]) ?? new User();
+        $user->setUsername($username);
+        $user->setRoles(['ROLE_ADMIN']);
+        $user->setPassword($this->passwordHasher->hashPassword($user, $password));
+
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+
+        $io->success(sprintf('Admin user "%s" is ready.', $username));
+
+        return Command::SUCCESS;
+    }
+}
